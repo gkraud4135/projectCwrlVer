063a7d677755c5735a10a4a7edc89a797f251166
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2021-12-15 20:37:32 +0900

    JsonViewAdd

---
+++ src/main/java/co/kr/project/main/MainController.java
@@ -0,0 +1,25 @@
+package co.kr.project.main;
+
+
+import co.kr.project.common.JsonView;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/main/")
+public class MainController {
+
+    @RequestMapping("test.do")
+    public View earIDupCheck(@RequestBody Map<String, String> paramMap, ModelMap model, HttpServletResponse response) throws Exception {
+
+
+
+        return new JsonView();
+    }
+}
